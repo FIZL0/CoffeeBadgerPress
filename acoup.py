08169a49e4725837d34dc076cac90257a2b06f05
FIZL0 <ryanhayden07@gmail.com>
2024-02-14 14:02:25 -0500

    added datetime

--- acoup.py
+++ acoup.py
@@ -17,6 +17,12 @@ for article in articles:
             newPage = requests.get(newURL)
             newSoup = BeautifulSoup(newPage.content, 'html.parser')
             newArticle = newSoup.find("div", class_="entry-content")
+            
+            time_tag = article.find("time", class_="entry-date published updated")
+            if time_tag:
+                # Print the content of the time tag
+                print("Date:", time_tag.text.strip())
+
             paragraphs = newArticle.find_all("p")
             print("Title:", title.text.strip())
             for paragraph in paragraphs:
