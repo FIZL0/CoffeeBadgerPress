eed53717c8bbfbce8c6ec4934d4a418ca76bf244
FIZL0 <ryanhayden07@gmail.com>
2024-02-14 13:44:11 -0500

    intial commit

---
+++ acoup.py
@@ -0,0 +1,24 @@
+import requests
+from bs4 import BeautifulSoup
+
+URL = "https://acoup.blog/"
+page = requests.get(URL)
+soup = BeautifulSoup(page.content, 'html.parser')
+
+articles = soup.find_all("article")
+for article in articles:
+    title = article.find("h1", class_="entry-title")
+    if title:
+        # Find the link within the title
+        link = title.find("a")
+        if link:
+            newURL = link.get("href")
+            print("Link:", newURL)
+            newPage = requests.get(newURL)
+            newSoup = BeautifulSoup(newPage.content, 'html.parser')
+            newArticle = newSoup.find("div", class_="entry-content")
+            paragraphs = newArticle.find_all("p")
+            print("Title:", title.text.strip())
+            for paragraph in paragraphs:
+                print(paragraph.text,"\n")
+            
