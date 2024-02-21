1d4d3e884c8be15e310c3f2be6f0b1dbbd678108
FIZL0 <ryanhayden07@gmail.com>
2024-02-21 10:35:18 -0500

    added main

--- acoup.py
+++ acoup.py
@@ -3,34 +3,35 @@ import dateparser
 
 from bs4 import BeautifulSoup
 
-URL = "https://acoup.blog/"
-page = requests.get(URL)
-soup = BeautifulSoup(page.content, 'html.parser')
+def getArticles():
+    URL = "https://acoup.blog/"
+    page = requests.get(URL)
+    soup = BeautifulSoup(page.content, 'html.parser')
 
-articles = soup.find_all("article")
-for article in articles:
-    title = article.find("h1", class_="entry-title")
-    if title:
-        # Find the link within the title
-        link = title.find("a")
-        if link:
-            newURL = link.get("href")
-            print("Link:", newURL)
-            newPage = requests.get(newURL)
-            newSoup = BeautifulSoup(newPage.content, 'html.parser')
-            newArticle = newSoup.find("div", class_="entry-content")
-            
-            time_tag = article.find("time", class_="entry-date published updated")
-            if time_tag is None:
-                time_tag = article.find("time", class_="entry-date published")
+    articles = soup.find_all("article")
+    for article in articles:
+        title = article.find("h1", class_="entry-title")
+        if title:
+            # Find the link within the title
+            link = title.find("a")
+            if link:
+                newURL = link.get("href")
+                print("Link:", newURL)
+                newPage = requests.get(newURL)
+                newSoup = BeautifulSoup(newPage.content, 'html.parser')
+                newArticle = newSoup.find("div", class_="entry-content")
 
-            if time_tag:
-                # Print the content of the time tag
-                parsed_date = dateparser.parse(time_tag.text.strip())
-                print("Date:", parsed_date)
+                time_tag = article.find("time", class_="entry-date published updated")
+                if time_tag is None:
+                    time_tag = article.find("time", class_="entry-date published")
 
-            paragraphs = newArticle.find_all("p")
-            print("Title:", title.text.strip())
-            for paragraph in paragraphs:
-                print(paragraph.text,"\n")
-            
+                if time_tag:
+                    # Print the content of the time tag
+                    parsed_date = dateparser.parse(time_tag.text.strip())
+                    print("Date:", parsed_date)
+
+                paragraphs = newArticle.find_all("p")
+                print("Title:", title.text.strip())
+                for paragraph in paragraphs:
+                    print(paragraph.text,"\n")
+    return
---
+++ main.py
@@ -0,0 +1,12 @@
+import acoup
+
+#given parameters
+start_date = 1/1/24
+end_date = 2/29/24
+
+acoup.getArticles()
+
+
+
+
+
