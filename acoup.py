728727245aae9246cf8d890860daaba8f26fc2e7
FIZL0 <ryanhayden07@gmail.com>
2024-02-21 10:54:05 -0500

    don't print articles out of range

--- acoup.py
+++ acoup.py
@@ -15,12 +15,6 @@ def getArticles(start_date, end_date):
             # Find the link within the title
             link = title.find("a")
             if link:
-                newURL = link.get("href")
-                print("Link:", newURL)
-                newPage = requests.get(newURL)
-                newSoup = BeautifulSoup(newPage.content, 'html.parser')
-                newArticle = newSoup.find("div", class_="entry-content")
-
                 time_tag = article.find("time", class_="entry-date published updated")
                 if time_tag is None:
                     time_tag = article.find("time", class_="entry-date published")
@@ -30,7 +24,13 @@ def getArticles(start_date, end_date):
                     parsed_date = dateparser.parse(time_tag.text.strip())
                     if parsed_date < start_date:
                         print("not in range:", parsed_date, start_date, end_date)
+                        continue
                     print("Date:", parsed_date)
+                newURL = link.get("href")
+                print("Link:", newURL)
+                newPage = requests.get(newURL)
+                newSoup = BeautifulSoup(newPage.content, 'html.parser')
+                newArticle = newSoup.find("div", class_="entry-content")
 
                 paragraphs = newArticle.find_all("p")
                 print("Title:", title.text.strip())
