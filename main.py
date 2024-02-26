004da3a6c9f812e8c06a7360855b31d0eeedec08
FIZL0 <ryanhayden07@gmail.com>
2024-02-26 11:07:38 -0500

    fix

--- main.py
+++ main.py
@@ -2,12 +2,14 @@ import acoup
 import insideofknoxville
 import datetime
 
+from newspaper import Newspaper
+
 start_date = datetime.datetime(2024, 1, 1)
 end_date = datetime.datetime(2024, 1, 15)
 
 newspaper = Newspaper()
 newspaper = acoup.getArticles(newspaper, start_date, end_date)
-insideofknoxville.getArticles(start_date, end_date)
+newspaper = insideofknoxville.getArticles(start_date, end_date)
 for article in newspaper.articles:
     # Access the attributes of each article
     article_date = article.date
