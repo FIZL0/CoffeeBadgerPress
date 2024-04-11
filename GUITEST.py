96d90533c5c298dcf8bbdb0ea404ad2de794a1c8
drewby2 <135929586+drewby2@users.noreply.github.com>
2024-04-11 02:25:16 -0400

    GUI update
    finalized design of GUI, not currently linked to main program. still might try and add an icon

--- GUITEST.py
+++ GUITEST.py
@@ -2,13 +2,28 @@ import tkinter as tk
 
 root = tk.Tk()
 
-root.geometry("800x500")
+root.geometry("400x290")
 root.title("Coffee Badger Press")
 
-label = tk.Label(root, text="Coffee Badger Press", font=('Arial', 18))
-label.pack(padx=20, pady=20)
+label1 = tk.Label(root, text="Coffee Badger Press", font=('Arial', 16))
+label1.pack(padx=20, pady=20)
 
-entry = tk.Entry(root)
+label2 = tk.Label(root, text="Print All Articles Since Last Print", font=('Arial', 14))
+label2.place(x=20,y=100)
+
+label3 = tk.Label(root, text="Print All Articles Since", font=('Arial', 14))
+label3.place(x=20,y=180)
+
+entry = tk.Entry(root, width=10)
 entry.pack()
+entry.place(x=220,y=185)
+
+button1 = tk.Button(root, text="Go", font=('Arial', 18))
+button1.pack(padx=300, pady=30)
+button1.place(x=310, y=90)
+
+button2 = tk.Button(root, text="Go", font=('Arial', 18))
+button2.pack(padx=300, pady=30)
+button2.place(x=310, y=170)
 
 root.mainloop()
