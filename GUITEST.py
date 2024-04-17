ec4c68fafa380d8ce4c42f3d7c397dd22680a604
drewby2 <135929586+drewby2@users.noreply.github.com>
2024-04-17 17:23:26 -0400

    Update GUITEST.py
    completed GUI design with buttons and date droppers

--- GUITEST.py
+++ GUITEST.py
@@ -1,4 +1,6 @@
 import tkinter as tk
+from tkcalendar import Calendar, DateEntry
+from tkinter import messagebox
 
 root = tk.Tk()
 
@@ -11,19 +13,34 @@ label1.pack(padx=20, pady=20)
 label2 = tk.Label(root, text="Print All Articles Since Last Print", font=('Arial', 14))
 label2.place(x=20,y=100)
 
-label3 = tk.Label(root, text="Print All Articles Since", font=('Arial', 14))
-label3.place(x=20,y=180)
+label3 = tk.Label(root, text="Print All Articles Between", font=('Arial', 14))
+label3.place(x=20,y=150)
 
-entry = tk.Entry(root, width=10)
-entry.pack()
-entry.place(x=220,y=185)
+label4 = tk.Label(root, text="And", font=('Arial', 14))
+label4.place(x=110,y=180)
 
-button1 = tk.Button(root, text="Go", font=('Arial', 18))
+cal = DateEntry(width= 8, background="magenta3", foreground="white",bd=2)
+cal.pack(pady=20)
+cal.place(x=160,y=185)
+
+cal2 = DateEntry(width= 8, background="magenta3", foreground="white",bd=2)
+cal2.pack(pady=20)
+cal2.place(x=30,y=185)
+
+def show_msg():
+    messagebox.showinfo("Test Message!")
+
+button1 = tk.Button(root, text="Go", command=show_msg, font=('Arial', 18))
 button1.pack(padx=300, pady=30)
 button1.place(x=310, y=90)
 
 button2 = tk.Button(root, text="Go", font=('Arial', 18))
 button2.pack(padx=300, pady=30)
-button2.place(x=310, y=170)
+button2.place(x=310, y=160)
+
+def get_dates():
+    firstDate = cal.get_date
+    secondDate = cal2.get_date
+    
 
 root.mainloop()
